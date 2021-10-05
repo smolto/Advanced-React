716cb9456203ec85135a1face8fe2102fe862995
Santiago Molto <sistemas1@persax.es>
2021-10-05 15:52:38 +0200

    Module 3 completed and Module 4 (till episode 15)

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,56 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://locahost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+    }),
+    ui: {
+      // Show the UI only for peaople who pass this test
+      isAccessAllowed: ({ session }) =>
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
